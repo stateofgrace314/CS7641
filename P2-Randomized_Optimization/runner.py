dd73ec9f40119feb5fde623f35b55848c4829fa3
Kyle Grace <kgrace6@gatech.edu>
2022-10-16 20:28:06 -0400

    finished NN and report

--- P2-Randomized_Optimization/runner.py
+++ P2-Randomized_Optimization/runner.py
@@ -5,14 +5,13 @@ Main script used to run all the experiments generating plots and data for P2
 from flipflop import runFlipFlop
 from fourpeaks import runFourPeaks
 from tsp import runTSP
-from knapsack import runKnapsack
-from onemax import runOneMax
+from nnoptimizer import runNNOptimizers
 
 
 runFourPeaks()
 runTSP()
 runFlipFlop()
-
+runNNOptimizers()
 # TODO: - Add custom parameter scaling per problem
 #   - Add capability to iterate over ranges of parameters (pop size for mimic and ga, temp for sa)
 #   - add NN stuff
